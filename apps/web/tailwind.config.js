fab8b7747f626fc2048a1be2bc588d0ebf643cf3
Shauls <shaul919191@gmail.com>
2025-12-13 21:40:57 +0200

    chore: Remove TypeScript declaration source map files.

--- apps/web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "../../packages/ui/src/**/*.{js,ts,jsx,tsx}",
-        "../../packages/code-editor/src/**/*.{js,ts,jsx,tsx}",
-    ],
-};
